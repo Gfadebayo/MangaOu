ec340adf53fb80f8e195073ea508b64d6381eb80
Fuad <gfadebayo16@gmail.com>
2021-03-05 12:00:50 +0100

    A custom navigator is now used to create and manage navigation fragment creation so only a single instance of each fragment except MangaFragment and EmptyFragment is created. The previous method in which we tried to pop the backstack is now removed. The navGraph is now set in code after creating the navigator due to the fact that the start destination ends up being created twice

--- app/src/main/java/com/exzell/mangaplayground/MainActivity.java
+++ app/src/main/java/com/exzell/mangaplayground/MainActivity.java
@@ -1,30 +1,24 @@
 package com.exzell.mangaplayground;
 
-import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.PermissionChecker;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.exzell.mangaplayground.databinding.ActivityDrawerBinding;
+import com.exzell.mangaplayground.fragment.EmptyFragment;
 import com.exzell.mangaplayground.fragment.MangaFragment;
-
-import com.google.android.material.navigation.NavigationView;
+import com.exzell.mangaplayground.navigator.SingleNavigator;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import timber.log.Timber;
 
@@ -44,24 +38,23 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration configBar = new AppBarConfiguration.Builder(R.id.nav_bookamrk)
                 .setOpenableLayout(mBinding.drawer).build();
 
+        Fragment host = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController.getNavigatorProvider()
+                .addNavigator(new SingleNavigator(this, host.getChildFragmentManager(), R.id.nav_host_fragment, MangaFragment.class.getName(), EmptyFragment.class.getName()));
+        navController.setGraph(R.navigation.nav_graph);
+
         NavigationUI.setupWithNavController(mBinding.activity.toolbar, navController, configBar);
-        setupNavViewWithNavController(mBinding.drawer, mBinding.navView, navController);
+        NavigationUI.setupWithNavController(mBinding.navView, navController);
 
         setLayoutBehaviour(navController);
 
-        if(getIntent().getAction().equals(Intent.ACTION_VIEW) && getIntent().hasCategory(Intent.CATEGORY_BROWSABLE)){
+        if (getIntent().getAction().equals(Intent.ACTION_VIEW) && getIntent().hasCategory(Intent.CATEGORY_BROWSABLE)) {
             String link = getIntent().getData().getPath();
             Timber.i("Path is %s", link);
             Bundle linkBundle = new Bundle(1);
             linkBundle.putString(MangaFragment.MANGA_LINK, link);
 
             navController.navigate(R.id.frag_manga, linkBundle);
-        }
-
-        if (BuildConfig.DEBUG) {
-            if (PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, Stream.of(Manifest.permission.WRITE_EXTERNAL_STORAGE).toArray(String[]::new), 0);
-            }
         }
     }
 
@@ -85,37 +78,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * This method will replace {@link NavigationUI#setupWithNavController(NavigationView, NavController)}
-     * as we intend to keep only 1 instance of each fragment but said method creates a new one each time
-     */
-    private void setupNavViewWithNavController(DrawerLayout drawer, NavigationView navView, NavController controller) {
-
-        navView.setNavigationItemSelectedListener(item -> {
-            boolean popBack = controller.popBackStack(item.getItemId(), false);
-            if (!popBack) popBack = NavigationUI.onNavDestinationSelected(item, controller);
-
-            if (popBack) drawer.close();
-
-            return popBack;
-        });
-
-        controller.addOnDestinationChangedListener((controller1, destination, arguments) ->
-                IntStream.range(0, navView.getMenu().size()).forEach(i -> {
-                    MenuItem item = navView.getMenu().getItem(i);
-                    item.setChecked(destination.getId() == item.getItemId());
-                }));
-    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (Arrays.asList(permissions).isEmpty()) return;
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public void onLowMemory() {
-        Timber.d("Warning!! Low memory");
     }
 }
 
---
+++ app/src/main/java/com/exzell/mangaplayground/navigator/SingleNavigator.java
@@ -0,0 +1,62 @@
+package com.exzell.mangaplayground.navigator;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentFactory;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.Navigator;
+import androidx.navigation.fragment.FragmentNavigator;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Navigator.Name("persist_fragment")
+public class SingleNavigator extends FragmentNavigator {
+
+    private Context mContext;
+    private FragmentManager mManger;
+    private int mContainerId;
+
+    //instances we want to keep
+    private List<String> mFragmentInstances;
+
+    //instances that have already been created
+    private Map<String, Fragment> mCreatedFragments = new HashMap<>();
+
+    public SingleNavigator(Context context, FragmentManager manager, int containerId, String... excludeInstances) {
+        super(context, manager, containerId);
+        mContext = context;
+
+        mManger = manager;
+        mContainerId = containerId;
+        mFragmentInstances = Arrays.asList(excludeInstances);
+
+        manager.setFragmentFactory(new Factory(manager.getFragmentFactory()));
+    }
+
+
+    public class Factory extends FragmentFactory {
+
+        FragmentFactory mDefaultFactory;
+
+        public Factory(FragmentFactory mDefaultFactory) {
+            this.mDefaultFactory = mDefaultFactory;
+        }
+
+        @NonNull
+        @Override
+        public Fragment instantiate(@NonNull ClassLoader classLoader, @NonNull String className) {
+            if (mCreatedFragments.containsKey(className)) return mCreatedFragments.get(className);
+
+            Fragment newInst = mDefaultFactory.instantiate(classLoader, className);
+
+            if (!mFragmentInstances.contains(className)) mCreatedFragments.put(className, newInst);
+
+            return newInst;
+        }
+    }
+}
